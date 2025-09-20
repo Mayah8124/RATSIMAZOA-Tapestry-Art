f95df0c92af6c4e3d881c9047f3da49e87fce757
mayahNeko <mayahibela@gmail.com>
2025-09-20 17:22:45 +0300

    chore: add root colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
+import { Router , Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
+    <div className=''>
       <p>coucou</p>
     </div>
   )
