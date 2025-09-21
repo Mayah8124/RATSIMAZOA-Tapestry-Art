4c20efa3e14edcb987f2590d6a10999cea081f1c
mayahNeko <mayahibela@gmail.com>
2025-09-21 19:02:31 +0300

    delete: remove scroll fonctionnality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,12 @@ export default function Home() {
                     by RATSIMAZOA
                 </h3>
                 <p className='text-title-secondary text-2xl pr-8 pl-[40vw]'>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur odit blanditiis quo quasi provident esse ut consequuntur officiis, repellendus, ratione perferendis similique alias, saepe dolorum necessitatibus? Porro dolore tenetur officia! 
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur odit blanditiis quo quasi provident esse ut consequuntur officiis, repellendus, ratione perferendis similique alias, saepe dolorum necessitatibus? Porro dolore tenetur officia!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In aliquid nisi, quia rem eveniet quos odio officiis labore, reiciendis suscipit laboriosam voluptates dolores dolorem, vero aspernatur excepturi sequi cum. Impedit?
+                    Aliquid suscipit blanditiis rem est accusamus perspiciatis tenetur voluptatem in? Ratione incidunt neque tempora ea sed, rem earum at maiores illo qui. Excepturi atque, provident fuga exercitationem iusto impedit quia!
+                    Omnis, laboriosam. Maiores quas ratione vero hic voluptatibus repudiandae inventore corporis facere unde vel qui tenetur doloribus aperiam ullam nobis quos necessitatibus optio recusandae, molestias molestiae quisquam. Fugiat, saepe deleniti!
+                    Aut, dicta explicabo beatae dignissimos, consequuntur impedit, omnis nihil repellat quod totam facilis numquam quisquam cumque? Incidunt, praesentium ea debitis labore molestiae consectetur. Aliquam quia nisi, a blanditiis ex totam.
+                    Ipsa rerum, id dolores, fugiat praesentium ex porro, sint perferendis est vel inventore temporibus cupiditate. Aliquam pariatur soluta, error dignissimos, praesentium laudantium possimus provident tempora consequuntur ipsam autem consequatur culpa. 
                 </p>
             </div>
         </div>
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,36 +1,20 @@
-import { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import logo from '../assets/logo.png'; 
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
-export default function Navbar() { 
-    const [scrolled, setScrolled] = useState(false);
-    const location = useLocation();
+export default function Navbar() {
+    return (
+        <div className='fixed top-0 w-full z-50 transition-colors duration-300'>
+            <div className="max-w-[1200px] mx-auto flex items-center justify-between px-4 py-2">
+                <img src={logo} alt="Tapestry Logo" className="h-[14vh] w-auto" />
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (location.pathname === '/Home' && window.scrollY === 0) {
-                setScrolled(false);
-            } else {
-                setScrolled(true); 
-            }
-        };
-
-    handleScroll();
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [location]);
-
-    return ( 
-    <div className='flex flex-2 flex-row gap-3 items-center px-4 fixed top-0 z-10'>    
-        <img src={logo} alt="Tapestry Logo" className="h-[10vh] w-auto" />
-        <nav className="bg-navbar-initial w-[85vw] py-3 px-7 rounded-2xl flex flew-row justify-around gap-4 items-center text-white-var font-serif text-ml">
-            <Link to="/Home" >Home</Link> 
-            <Link to='/About'>About</Link> 
-            <Link to='/Product'>Product</Link> 
-            <Link to='/Review'>Review</Link> 
-            <Link to='/Contact'>Contact</Link>
-        </nav>
-    </div>
-    )
+                <nav className='bg-navbar-initial  w-[85vw] py-3 px-7 rounded-2xl flex flex-row justify-around gap-4 items-center font-serif text-ml text-title-secondary'>
+                    <Link to="/Home">Home</Link>
+                    <Link to="/About">About</Link>
+                    <Link to="/Product">Product</Link>
+                    <Link to="/Review">Review</Link>
+                    <Link to="/Contact">Contact</Link>
+                </nav>
+            </div>
+        </div>
+    );
 }
