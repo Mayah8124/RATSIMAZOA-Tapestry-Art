84e5af7650f72808b58c553e95c42f4bb3603884
mayahNeko <mayahibela@gmail.com>
2025-09-24 13:52:06 +0300

    feat: add customer data file

---
+++ src/data/customers.js
@@ -0,0 +1,27 @@
+export const customers = [
+    {
+        picture: '/assets/customer/anonyme.jpeg',
+        name: "Anonyme customer",
+        review: "Beautiful work, the details are perfect! You can feel the passion behind it."
+    },
+    {
+        picture: '/assets/customer/cat.jpeg',
+        name: "Ophelia Smith",
+        review: "I absolutely loved my purchase, the crochet is high quality and so pretty. Thank you!"
+    },
+    {
+        picture: '/assets/customer/chill_girl.jpeg',
+        name: "Maria Johns",
+        review: "Fast delivery and the product looks even better than in the pictures. Highly recommended!"
+    },
+    {
+        picture: '/assets/customer/girl.jpeg',
+        name: "Ornely Stone",
+        review: "Such a lovely piece 😍 handmade, unique, and carefully crafted down to the smallest detail"
+    },
+    {
+        picture: '/assets/customer/polar.jpeg',
+        name: "Orion Gomez",
+        review: "Very happy with my order, it’s soft, sturdy, and original. True craftsmanship"
+    }
+];
