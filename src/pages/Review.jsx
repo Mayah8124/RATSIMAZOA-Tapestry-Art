c82f3bca78d14932530817c60cb3eb566694a8e6
mayahNeko <mayahibela@gmail.com>
2025-09-24 16:18:39 +0300

    feat: add review cards

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -2,7 +2,7 @@ import { customers } from "../data/customers"
 
 export default function Review() {
     return (
-        <div>
+        <div className="py-10">
             <div className='flex flex-col gap-2 text-center font-serif font-stretch-expanded py-5'>
                 <h1 className="text-title-primary text-7xl font-bold">
                     Reviews
@@ -11,22 +11,25 @@ export default function Review() {
                     More the client are satisfied more I am proud of myself
                 </p>
             </div>
-            <div className="mt-10">
+            <div className="mt-10 px-5 grid grid-cols-4 justify-around">
                 {
                     customers.map((c) => {
-                        <div>
-                            <div>
-                                <img src={c.picture} alt="customer_picture" />
+                        return (
+                            <div className="w-[21vw] h-[48vh] bg-fourth-color rounded-xl shadow-md m-3
+                                            transition-transform duration-200 hover:scale-110">
+                                <div className="rounded-t-xl bg-third-color p-2 flex justify-center items-center">
+                                    <img src={c.picture} alt="customer_picture" className=" w-[50%] object-cover rounded-full" />
+                                </div>
+                                <div className="bg-fourth-color flex flex-col gap-2 font-serif font-stretch-condensed text-center p-4 rounded-b-xl">
+                                    <h1 className="text-xl text-title-secondary font-semibold">
+                                        {c.name}
+                                    </h1>
+                                    <p>
+                                        "{c.review}"
+                                    </p>
+                                </div>
                             </div>
-                            <div>
-                                <h1>
-                                    {c.name}
-                                </h1>
-                                <p>
-                                    {c.review}
-                                </p>
-                            </div>
-                        </div>
+                        )
                     })
                 }
             </div>
