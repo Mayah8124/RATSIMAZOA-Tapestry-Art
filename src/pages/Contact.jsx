4afede34f2acfbed6fc06a64b385f32578dfc645
mayahNeko <mayahibela@gmail.com>
2025-09-25 11:15:54 +0300

    chore: install react-icon

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,17 @@
+import { FaPhone } from "react-icons/fa"
+
 export default function Contact() {
     return (
-        <div className="bg-fourth-color">
-            
+        <div className="bg-fourth-color grid grid-cols-2">
+            <div className="text-left">
+                {/* react icon Localisation "where to find use" */}
+                <h1>It's a phone
+                    <FaPhone className="text-red-700"/>
+                </h1>
+            </div>
+            <div className="">
+                {/* cellphone and facebook account add link */}
+            </div>
         </div>
     )
 }
