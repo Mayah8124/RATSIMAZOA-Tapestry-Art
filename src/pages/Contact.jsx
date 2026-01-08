b21c4840f9819eb7b7b7ac2ce16f33ecd6f9e74e
mayahNeko <mayahibela@gmail.com>
2026-01-08 17:26:48 +0300

    feat: add all contacts

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,23 +1,22 @@
 import { FaPhone, FaFacebook } from "react-icons/fa"
 import { MdLocationOn } from "react-icons/md"
 import { FaRegCopy } from "react-icons/fa";
-import { useState , useRef } from "react";
+import { IoIosCheckmarkCircleOutline } from "react-icons/io";
+import { useState  } from "react";
 
 export default function Contact() {
-    const [textToCopy, setTextToCopy] = useState('Some default text to copy');
+    const [textToCopy, setTextToCopy] = useState('Tapestry Art By RATSIMAZOA');
+    const [numberToCopy, setNumberCopy] = useState('+261 34 61 716 62')
     const [copySuccess, setCopySuccess] = useState('');
-    const inputRef = useRef(null);
 
     const copyToClipboard = async () => {
         try {
-        // Use the modern Clipboard API
             await navigator.clipboard.writeText(textToCopy);
-            setCopySuccess('Copied!');
+            await navigator.clipboard.writeText(numberToCopy);
+            setCopySuccess(true);
       
-        // Optional: Reset the success message after a few seconds
-            setTimeout(() => {
-                setCopySuccess('');
-            }, 2000); // 2 seconds
+            setTimeout(() => 
+                setCopySuccess(false), 2000); 
       
         } catch (err) {
             console.error('Failed to copy text: ', err);
@@ -26,40 +25,29 @@ export default function Contact() {
     };
 
     return (
-        <div className="bg-fourth-color grid grid-cols-2 text-title-primary font-semibold font-serif">
-            <div className="text-left ">
-                {/* react icon Localisation "where to find use" */}
-                <h1 className="flex flex-row-reverse justify-center items-center">
-                    <span>
-                        Ivato
-                    </span>
-                    <MdLocationOn className="text-title-secondary text-[40px]"/>
-                </h1>
-                <h1>
+        <div className="bg-fourth-color grid grid-cols-3 text-title-primary font-semibold font-serif py-2">
+            {/* react icon Localisation "where to find us" */}
+            <h1 className="flex flex-row-reverse justify-center items-center gap-1">
+                <span>
+                    Ivato, 105
+                </span>
+                <MdLocationOn className="text-title-secondary text-[40px]"/>
+            </h1>
+        
+            {/* cellphone and facebook account add link */}
+            <h1 className="flex flex-row-reverse justify-center items-center gap-3">
+                <span>
+                    +261 34 61 716 62
+                </span>
+                <FaPhone className="text-title-secondary text-[30px]"/>
+            </h1>
 
-                </h1>
-            </div>
-            <div className="">
-                {/* cellphone and facebook account add link */}
-                <h1>
-                    <FaRegCopy className="text-title-secondary text-[30px]"/>
-                    <span
-                        type="text"
-                        ref={inputRef}
-                        value={textToCopy}
-                        onChange={(e) => setTextToCopy(e.target.value)}
-                    >
-                        +261 34 --- -- ---
-                    </span>
-                    <FaPhone className="text-title-secondary text-[30px]"/>
-                </h1>
-                <h1>
-                    <span>
-
-                    </span>
-                    <FaFacebook className="text-title-secondary text-[30px]"/>
-                </h1>
-            </div>
+            <h1 className="flex flex-row-reverse justify-center items-center gap-3">
+                <span>
+                    Tapestry Art By RATSIMAZOA
+                </span>
+                <FaFacebook className="text-title-secondary text-[30px]"/>
+            </h1>
         </div>
     )
 }
