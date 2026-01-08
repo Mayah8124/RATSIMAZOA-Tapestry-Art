cf4ccf341c19667a9d5e77719257293ae76eb659
mayahNeko <mayahibela@gmail.com>
2026-01-08 10:22:12 +0300

    feat: add contact field

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,30 @@
-import { FaPhone } from "react-icons/fa"
+import { FaPhone, FaFacebook } from "react-icons/fa"
+import { MdLocationOn } from "react-icons/md"
 
 export default function Contact() {
     return (
-        <div className="bg-fourth-color grid grid-cols-2">
-            <div className="text-left">
+        <div className="bg-fourth-color grid grid-cols-2 text-title-primary font-semibold font-serif">
+            <div className="text-left ">
                 {/* react icon Localisation "where to find use" */}
-                <h1>It's a phone
-                    <FaPhone className="text-red-700"/>
+                <h1 className="flex flex-row-reverse justify-center">
+                    <span>
+                        Ivato
+                    </span>
+                    <MdLocationOn className="text-title-secondary text-[40px]"/>
+                </h1>
+                <h1>
+
                 </h1>
             </div>
             <div className="">
                 {/* cellphone and facebook account add link */}
+                <h1>
+                    It's a phone
+                    <FaPhone className="text-title-secondary text-[30px]"/>
+                </h1>
+                <h1>
+                    <FaFacebook className="text-title-secondary text-[30px]"/>
+                </h1>
             </div>
         </div>
     )
