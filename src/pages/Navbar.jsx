1c27f0e6392644952e7213711b17e45cf9ab761f
mayahNeko <mayahibela@gmail.com>
2026-01-29 18:30:05 +0300

    chore(navbar): change navigation

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,18 +1,30 @@
-import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 export default function Navbar() {
-    return (
-        <div className='fixed top-0 w-full z-50 transition-colors duration-300'>
-            <div className="max-w-[1200px] mx-auto flex items-center justify-between px-4 py-2">
-                <img src={logo} alt="Tapestry Logo" className="h-[14vh] w-auto" />
 
-                <nav className='bg-navbar-initial  w-[85vw] py-3 px-7 rounded-2xl flex flex-row justify-around gap-4 items-center font-serif text-ml text-title-secondary'>
-                    <Link to="/Home">Home</Link>
-                    <Link to="/About">About</Link>
-                    <Link to="/Product">Product</Link>
-                    <Link to="/Review">Review</Link>
-                    <Link to="/Contact">Contact</Link>
+    const scrollToSection = (id) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
+    return (
+        <div className="fixed top-0 w-full z-50">
+            <div className="max-w-[1200px] mx-auto flex items-center justify-between px-4 py-2">
+                <img
+                    src={logo}
+                    alt="Tapestry Logo"
+                    className="h-[14vh] w-auto cursor-pointer"
+                    onClick={() => scrollToSection('home')}
+                />
+
+                <nav className="bg-navbar-initial w-[85vw] py-3 px-7 rounded-2xl flex justify-around items-center font-serif text-ml text-title-secondary">
+                    <button onClick={() => scrollToSection('home')}>Home</button>
+                    <button onClick={() => scrollToSection('about')}>About</button>
+                    <button onClick={() => scrollToSection('product')}>Product</button>
+                    <button onClick={() => scrollToSection('review')}>Review</button>
+                    <button onClick={() => scrollToSection('contact')}>Contact</button>
                 </nav>
             </div>
         </div>
